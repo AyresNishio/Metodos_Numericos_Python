ae73aa291fc0b9dbbd9f066b2c099e5aae26d68a
ViniBiajoni <vinibiajoni@gmail.com>
2021-10-02 09:55:10 -0300

    Machine Precision numpy

---
+++ .history/Cap2/machine_precision_20211002095315.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+#Precisão Máquina Pyhon (Biblioteca Numpy)
+
--- .history/Cap2/machine_precision_20211002095429.py
+++ .history/Cap2/machine_precision_20211002095429.py
@@ -4,15 +4,15 @@ import numpy as np
 print("Float:\n")
 print(np.finfo(float).eps)
 
-print("Float 32 bits:\n")
+print("Float:\n")
 print(np.finfo(np.float32).eps)
 
-print("Float 64 bits:\n")
+print("Float:\n")
 print(np.finfo(np.float64).eps)
 
-print("Float 128 bits:\n")
+print("Float:\n")
 print(np.finfo(np.float128).eps)
 
 
 #Precisão Máquina Algoritmo
- 
+#  
